1c4c1cdf1c00612bfcfd6dbcac9f6067a57294b5
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-08-13 09:56:29 +0930

    Setup boilerplate code
    - Next.js Frontend
    - Apollo data layer
    - Django Backend
    - Graphene-Django GraphQL API
    - Postgres DB

---
+++ frontend/pages/api/apollo-client.js
@@ -0,0 +1,14 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
