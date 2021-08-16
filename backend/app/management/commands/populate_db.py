0a5750c746fd72d53c9b5f69e3f000dd4771d99d
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-08-16 11:36:19 +0930

    income statements populated and working

--- backend/app/management/commands/populate_db.py
+++ backend/app/management/commands/populate_db.py
@@ -37,21 +37,32 @@ class Command(BaseCommand):
         # Load API Key from environment variables
         MY_API_KEY = os.environ.get("MY_API_KEY")
 
+        # Function to perform API call and parse JSON
         def get_jsonparsed_data(url):
             response = urlopen(url)
             data = response.read().decode("utf-8")
             return json.loads(data)
 
-        tickers = ["AAPL", "TSLA", "NVDA"]
+        # List of Tickers to populate DB for
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        s_and_p_df = pd.read_csv(
+            dir_path+"/SAndP500.csv")
 
-        for ticker in tickers:
+        # Add list to database
+        # s_and_p_df.to_sql(
+        #     "app_sandp500", con=engine, if_exists='replace', index=False)
+
+        # API Call for Income Statements
+        income_statement_df = pd.DataFrame([])
+        for index, symbol, in enumerate(s_and_p_df['symbol']):
+            print("Fetching Income Statement for: ", symbol)
+            print(str(index) + " of " + str(len(s_and_p_df['symbol'])))
+            print("---------------------------------------------------------")
             url = (
-                f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?apikey={MY_API_KEY}")
-            stock_data = get_jsonparsed_data(url)
-            print(stock_data)
-            stock_data_frame = pd.DataFrame(data=stock_data)
-            stock_data_frame['key'] = stock_data_frame['symbol'] + \
-                ": " + stock_data_frame['date']
-            print(stock_data_frame)
-            stock_data_frame.to_sql(
-                "app_incomestatement", con=engine, if_exists='replace')
+                f"https://financialmodelingprep.com/api/v3/income-statement/{symbol}?limit=3&apikey={MY_API_KEY}")
+            symbol_data = get_jsonparsed_data(url)
+            symbol_data_df = pd.DataFrame(data=symbol_data)
+            income_statement_df = income_statement_df.append(symbol_data_df)
+
+        income_statement_df.to_sql(
+            "app_incomestatement", con=engine, if_exists='replace', index=False)
